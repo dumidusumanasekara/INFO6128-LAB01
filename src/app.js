b9190e1a42564d255736544e90fb2d8f0b8795c2
dumidusumanasekara <dumidusumanasekara@gmail.com>
2022-01-27 17:50:43 -0500

    1st

---
+++ src/app.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const parsedUrl = new URL(window.location);
+    // searchParams.get() will properly handle decoding the values.
+    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+  });
+
+
+
